9dbf8f22db3ba3b626f627ea33bcffb6be44f86c
maurwtz <mauri_wet@hotmail.com>
2023-03-27 18:48:46 -0300

    Rewrite
    El boton de instalar se esconde si ya está instalado

--- js/tetris.js
+++ js/tetris.js
@@ -260,7 +260,7 @@ function clearAllIntervals(){
     clearInterval( interval );
     clearInterval( intervalRender );
 }
-
+/*
 const butInstall = document.getElementById("butInstall");
 
 let prompt;
@@ -275,3 +275,39 @@ butInstall.addEventListener('click', function(){
     console.log("estoy aca")
    prompt.prompt();
 })
+*/
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent the mini-infobar from appearing on mobile.
+    event.preventDefault();
+    console.log('👍', 'beforeinstallprompt', event);
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Remove the 'hidden' class from the install button container.
+    divInstall.classList.toggle('hidden', false);
+  });
+
+  butInstall.addEventListener('click', async () => {
+    console.log('👍', 'butInstall-clicked');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log('👍', 'userChoice', result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    divInstall.classList.toggle('hidden', true);
+  });
+
+  window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+    // Clear the deferredPrompt so it can be garbage collected
+    window.deferredPrompt = null;
+  });
