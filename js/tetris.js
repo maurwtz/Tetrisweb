6bcc182f91ae8e04f91a346c6241497fc80c8026
maurwtz <mauri_wet@hotmail.com>
2023-03-27 18:09:21 -0300

    Test
    test

--- js/tetris.js
+++ js/tetris.js
@@ -260,7 +260,7 @@ function clearAllIntervals(){
     clearInterval( intervalRender );
 }
 
-let deferredPrompt;
+/*let deferredPrompt;
     window.addEventListener('beforeinstallprompt', (e) => {
         deferredPrompt = e;
     });
@@ -274,4 +274,35 @@ let deferredPrompt;
                 deferredPrompt = null;
             }
         }
-    });
+    });*/
+
+    // Inicializa deferredPrompt para su uso más tarde.
+
+const divInstall = document.getElementById("installContainer");
+const butInstall = document.getElementById("butInstall");
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Previene a la mini barra de información que aparezca en smartphones
+  e.preventDefault();
+  // Guarda el evento para que se dispare más tarde
+  deferredPrompt = e;
+  // Actualizar la IU para notificarle al usuario que se puede instalar tu PWA
+  showInstallPromotion();
+  // De manera opcional, envía el evento de analíticos para saber si se mostró la promoción a a instalación del PWA
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
+buttonInstall.addEventListener('click', async () => {
+    // Esconde la información promotora de la instalación
+    hideInstallPromotion();
+    // Muestre el mensaje de instalación
+    deferredPrompt.prompt();
+    // Espera a que el usuario responda al mensaje
+    const { outcome } = await deferredPrompt.userChoice;
+    // De manera opcional, envía analíticos del resultado que eligió el usuario
+    console.log(`User response to the install prompt: ${outcome}`);
+    // Como ya usamos el mensaje, no lo podemos usar de nuevo, este es descartado
+    deferredPrompt = null;
+  });
