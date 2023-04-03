a5aa542b7d53173daf76a8c6b42a3c7ad49016d6
maurwtz <mauri_wet@hotmail.com>
2023-04-03 17:28:59 -0300

    Added buttons
    se agregaron botones par soporte movil

--- js/controller.js
+++ js/controller.js
@@ -6,8 +6,35 @@ document.body.onkeydown = function( e ) {
         38: 'rotate',
         32: 'drop'
     };
+
+
     if ( typeof keys[ e.keyCode ] != 'undefined' ) {
         keyPress( keys[ e.keyCode ] );
         render();
     }
 };
+
+
+let btnRotate = document.getElementById("butRotar");
+let btnDown = document.getElementById("butAbajo");
+let btnRight = document.getElementById("butDerecha");
+let btnLeft = document.getElementById("butIzquierda");
+
+
+btnDown.addEventListener("click", function( e )  {
+    
+    keyPress( 'down' );
+    render();
+});
+btnRight.addEventListener("click", () => {
+    keyPress( 'right');
+    render();
+});
+btnLeft.addEventListener("click", () => {
+    keyPress( 'left');
+    render();
+});
+btnRotate.addEventListener("click", () => {
+    keyPress( 'rotate');
+    render();
+});
