49bd7fbcab35148afa10af6308f9c7e4c7e4c605
maurwtz <mauri_wet@hotmail.com>
2023-05-22 15:37:19 -0300

    Nuevos valores alpha beta gamma

--- js/tetris.js
+++ js/tetris.js
@@ -82,19 +82,19 @@ function handleOrientation(event) {
   // Use the rotation data to control Tetris movement
   // Implement your logic to move the Tetris piece based on orientation data
   // For example:
-  if (gamma || alpha > 0) { // 0 browsre - 30 phone
+  if (gamma || alpha < 65) { // 0 browsre - 30 phone
     console.log("Right")
     // Tilted to the right
     moveRight(); // Move Tetris piece right
-  } else if (gamma || alpha < -30) { // 0 browser - -30 phone
+  } else if (gamma || alpha < 100) { // 0 browser - -30 phone
     console.log("Left")
     // Tilted to the left
     moveLeft(); // Move Tetris piece left
-  } else if (beta > 120) { // 90 browser - 120 phone
+  } else if (beta > 15) { // 90 browser - 120 phone
     console.log("Down")
     // Tilted down
     moveDown(); // Move Tetris piece down
-  } else if (beta < 70) { // 90 browser - 70 phone
+  } else if (beta < 5) { // 90 browser - 70 phone
     console.log("Up")
     // Tilted up
     rotate(); // Rotate Tetris piece
